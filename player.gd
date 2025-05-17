3ca04a712b967dc59188bf1e6127eb690c8d3f07
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-05-18 03:05:02 +0700

    0.3.5

--- player.gd
+++ player.gd
@@ -3,8 +3,24 @@ var max_speed = 200
 func _process(delta):
 	var direction = movement_vector().normalized()
 	velocity = max_speed * direction
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false	
+		$AnimatedSprite2D.play("run_right")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true	
+		$AnimatedSprite2D.play("run_right")
+	elif velocity.y > 0:
+		
+		$AnimatedSprite2D.play("run_down")
+	elif velocity.y < 0:
+		
+		$AnimatedSprite2D.play("run_up")
+	else:
+		$AnimatedSprite2D.play("idle")
 	move_and_slide()
+	
 func movement_vector():
 	var movement_x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var movement_y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	return Vector2(movement_x,movement_y)
+	
