7d7eb25f1a9f6b7601bb69f58b33ba2ebe30d80a
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-05-19 06:27:13 +0700

    Update generator.gd

--- scene/generator/generator.gd
+++ scene/generator/generator.gd
@@ -3,14 +3,14 @@ extends StaticBody2D
 
 
 func _on_generator_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player")or body.is_in_group("coins"):
 		$Sprite.play("buid_up")
 		
 		
 
 
 func _on_generator_area_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player")  or body.is_in_group("coins"):
 		$Sprite.play_backwards("buid_up")
 
 
