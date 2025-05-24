61d47ec0b2ba74be1daaae4a79448ed8e483a4c9
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-05-24 19:40:42 +0700

    mojno_stroit

--- scene/player/player.gd
+++ scene/player/player.gd
@@ -1,24 +1,36 @@
 extends CharacterBody2D
 var max_speed = 100
 @onready var cave: = $/root/Main/Cave
-var damage = 5 
+var damage = 3
+var	build_mode = false
+var	fight_mode = false
+var	dig_mode = false 
+var dig_av= true
+var build_sprite =  preload("res://art/pics/otv.png")
+#var fight_sprite =  preload()
+var dig_sprite =  preload("res://scene/laser/sprite/laser_use4.png")
 
 func _physics_process(delta: float) -> void:
+	
 	var direction = movement_vector().normalized()
 	velocity = max_speed * direction * delta * max_speed
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false	
 		$AnimatedSprite2D.play("run_right")
 		$RayCast2D.rotation = deg_to_rad(-90)
+
 	elif velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true	
 		$AnimatedSprite2D.play("run_right")
 		$RayCast2D.rotation = deg_to_rad(90)
+		
 	elif velocity.y > 0:
 		$AnimatedSprite2D.play("run_down")
 		$RayCast2D.rotation = deg_to_rad(0)
+
 	elif velocity.y < 0:
 		$RayCast2D.rotation = deg_to_rad(180)
+
 		$AnimatedSprite2D.play("run_up")
 	else:
 		$AnimatedSprite2D.play("idle")
@@ -31,13 +43,21 @@ func _physics_process(delta: float) -> void:
 		$Camera2D.zoom-= Vector2(0.1,0.1)
 	if Input.is_action_just_pressed("zoom_reset"):
 		$Camera2D.zoom = Vector2(1.75,1.75)
-	if Input.is_action_just_pressed("use"):
-		if $RayCast2D.get_collider() and $RayCast2D.get_collider().get("name") == "Cave":
-			var colider = $RayCast2D.get_collider_rid()
-			cave.damage_tile(colider,damage)
-
-		else: return
-	if Input.is_action_pressed("rmb"):
+	if Input.is_action_pressed("use"):
+		if dig_mode ==true and dig_av== true:	
+			if $RayCast2D.get_collider() and $RayCast2D.get_collider().get("name") == "Cave":
+				var colider = $RayCast2D.get_collider_rid()
+				cave.damage_tile(colider,damage)
+				dig_av= false
+				$dig_timer.start()
+			
+			else: return
+	if Input.is_action_just_pressed("dig_mode"):
+		if dig_mode ==false:
+			set_mode("1")
+		else:
+			set_mode("0")
+	if Input.is_action_pressed("rmb") and dig_mode ==true:
 		$suck/CollisionShape2D.disabled =false
 		$GPUParticles2D.emitting = true
 	else:
@@ -47,5 +67,28 @@ func movement_vector():
 	var movement_x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var movement_y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	return Vector2(movement_x,movement_y) 
+func set_mode(mode):
+	if mode == "1":
+		build_mode = false
+		fight_mode = false
+		dig_mode = true
+		$Tool.texture = dig_sprite
+	if mode == "2":
+		build_mode = false
+		fight_mode = true
+		dig_mode = false
+		$Tool.texture = null
+	if mode == "3":
+		build_mode = true
+		fight_mode = false
+		dig_mode = false
+		$Tool.texture = build_sprite
+	if mode == "0":
+		build_mode = false
+		fight_mode = false
+		dig_mode = false
+		$Tool.texture = null
 
-	
+
+func _on_dig_timer_timeout() -> void:
+	dig_av= true
---
+++ scene/turret/turret.gd
@@ -0,0 +1,3 @@
+extends StaticBody2D
+var building_sprite= preload("res://scene/turret/sprite/turret.png")
+var price = 3
